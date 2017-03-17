2efc6b9537d7bdec20859d232f143eb0460ce054
adam <apk@pluvi.us>
2017-03-17 11:24:43 -0400

    add proper broadcasts

--- genarr.cpp
+++ genarr.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int*
 genarr(int n) {
   srand(time(0));
-  int *a = new int[n];
+  int*  a = new int[n];
   for (int i = 0; i < n; i++)
     a[i] = (rand() % 100);
   return a;
--- main.cpp
+++ main.cpp
@@ -20,20 +20,30 @@ main(int argc, char **argv) {
   if (my_rank == 0) {
     cout << "n: ";
     cin >> n;
+    /* broadcast n to all processors */
+    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WOLRD);
     a = genarr(n);
     cout << "unsorted:" << endl;
-    
+    cout << "rank: " << my_rank << ": ";
     for(int i = 0; i < n; i++)
-      cout << "a[" << i << "]: " << a[i] << endl;
-
+      cout << a[i] << " ";
     /* broadcast array to all processors */
     MPI_Bcast(a, n, MPI_INT, 0, MPI_COMM_WORLD);
+  }
+  else {
+    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WOLRD);
+    a = new int[n];
+    /* broadcast array to all processors */
+    MPI_Bcast(a, n, MPI_INT, 0, MPI_COMM_WORLD);
+    cout << "rank: " << my_rank << ": ";
+    for(int i = 0; i < n; i++)
+      cout << a[i] << " ";
+  }
     mergesort(a, 0, n - 1);
     cout << "sorted:" << endl;
 
     for(int i = 0; i < n; i++)
       cout << "a[" << i << "]: " << a[i] << endl;
-  }
 
   MPI_Finalize();
   return 0;
